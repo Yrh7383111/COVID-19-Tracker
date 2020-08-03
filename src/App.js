8dbf7d3a404658a50480be9d3ff91c7c960e4070
Jackson Yin <Yrh7383111@outlook.com>
2020-08-03 18:09:20 -0400

    Complete the project

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import React, { useState, useEffect} from 'react';
+import React, { useState, useEffect } from "react";
 import { FormControl, Select, MenuItem, Card, CardContent } from "@material-ui/core";
 import InfoBox from "./components/info-box/info-box.component";
 import Map from "./components/map/map.component";
 import Table from "./components/table/table.component";
-import { sortData } from "./utility/utility";
+import LineChart from "./components/line-chart/line-chart.component";
+import { printPrettyStats, sortData } from "./utility/utility";
 import './App.css';
+import "leaflet/dist/leaflet.css"
 
 
 
@@ -14,21 +16,30 @@ function App() {
     const [country, setCountry] = useState('worldwide');
     const [countryInfo, setCountryInfo] = useState({});
     const [tableData, setTableData] = useState([]);
+    const [mapCenter, setMapCenter] = useState({ lat: 34.80746, lng: -40.4796 });
+    const [mapZoom, setMapZoom] = useState(3);
+    const [mapCountries, setMapCountries] = useState([]);
+    const [casesType, setCasesType] = useState('cases');
 
 
 
     // Use effect for countryInfo - runs when the component first mounts
-    useEffect(() => {
-        fetch("https://disease.sh/v3/covid-19/all")
-            .then((response) => response.json())
-            .then((data) => {
-                setCountryInfo(data);
-            });
+    useEffect(async () => {
+        const getCountryInfo = async () => {
+            await fetch('https://disease.sh/v3/covid-19/all')
+                .then((response) => response.json())
+                .then((data) => {
+                    setCountryInfo(data);
+                });
+        };
+
+        getCountryInfo();
     }, []);
+
 
     // Use effect for countries - runs when the component first mounts
     useEffect(() => {
-        const getCountriesData = async () => {
+        const getCountries = async () => {
              await fetch('https://disease.sh/v3/covid-19/countries')
                 .then(response => response.json())
                 .then(data => {
@@ -41,11 +52,12 @@ function App() {
                     const sortedData = sortData(data);
                     setTableData(sortedData);
                     setCountries(countries);
+                    setMapCountries(data);
                 });
         };
 
         // Call the function
-        getCountriesData();
+        getCountries();
     }, []);
 
 
@@ -66,6 +78,8 @@ function App() {
             .then(data => {
                 setCountry(countryCode);
                 setCountryInfo(data);
+                setMapCenter([data.countryInfo.lat, data.countryInfo.long]);
+                setMapZoom(4);
             });
     };
 
@@ -95,30 +109,48 @@ function App() {
             {/* Information box */}
             <div className="info-box">
                 {/* Information box - Total case */}
-                <InfoBox title='Total Cases' cases={countryInfo.todayCases} total={countryInfo.cases} />
+                <InfoBox onClick={(event) => setCasesType('cases')}
+                         title='Today Cases'
+                         active={casesType === 'cases'}
+                         isRed
+                         cases={printPrettyStats(countryInfo.todayCases)}
+                         total={printPrettyStats(countryInfo.cases)} />
 
                 {/* Information box - Total recovered */}
-                <InfoBox title='Recovered' cases={countryInfo.todayRecovered} total={countryInfo.recovered} />
+                <InfoBox onClick={(event) => setCasesType('recovered')}
+                         title='Today Recovered'
+                         active={casesType === 'recovered'}
+                         cases={printPrettyStats(countryInfo.todayRecovered)}
+                         total={printPrettyStats(countryInfo.recovered)} />
 
                 {/* Information box - Total death */}
-                <InfoBox title='Deaths' cases={countryInfo.todayDeaths} total={countryInfo.deaths} />
+                <InfoBox onClick={(event) => setCasesType('deaths')}
+                         title='Today Deaths'
+                         active={casesType === 'deaths'}
+                         isRed
+                         cases={printPrettyStats(countryInfo.todayDeaths)}
+                         total={printPrettyStats(countryInfo.deaths)} />
             </div>
 
 
             {/* Map */}
-            <Map />
+            <Map countries={mapCountries} casesType={casesType} center={mapCenter} zoom={mapZoom} />
         </div>
 
 
 
         <Card className='right'>
             <CardContent>
-                {/* Tables */}
-                <h3>Live Cases by Country</h3>
-                <Table countries={tableData} />
+                <div className='information'>
+                    {/* Tables */}
+                    <h3>Live Cases by Country</h3>
+                    <Table countries={tableData} />
 
-                {/* Line chart */}
-                <h3>Worldwide new Cases</h3>
+
+                    {/* Line chart */}
+                    <h3>Worldwide new {casesType}</h3>
+                    <LineChart casesType={casesType} />
+                </div>
             </CardContent>
         </Card>
     </div>
--- src/components/info-box/info-box.component.jsx
+++ src/components/info-box/info-box.component.jsx
@@ -1,22 +1,24 @@
 import React from "react";
 import { Card, CardContent, Typography } from "@material-ui/core";
+import './info-box.styles.css'
 
 
 
 // Information box component
 // Object destructing
-function InfoBox({ title, cases, total }) {
+function InfoBox({ title, active, isRed, cases, total, ...props }) {
     return (
-        <Card>
+        <Card className={`info-box ${active && 'info-box-selected'} ${isRed && 'info-box-red'}`}
+              onClick={props.onClick}>
             <CardContent>
                 {/* Title */}
                 <Typography classes='title' color='textSecondary'>{title}</Typography>
 
                 {/* Cases */}
-                <h2 className='cases'>{cases}</h2>
+                <h2 className={`info-box-cases ${!isRed && 'info-box-cases-green'}`}>{cases}</h2>
 
                 {/* Total */}
-                <Typography className='total' color='textSecondary'>{total} Total</Typography>
+                <Typography className='info-box-total' color='textSecondary'>{total} Total</Typography>
             </CardContent>
         </Card>
     );
